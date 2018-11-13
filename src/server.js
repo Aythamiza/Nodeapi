af2092793f5b6ec23f2eac893a15fdbb9c06150d
Aythami <aythamiza@gmail.com>
2018-11-13 18:05:01 +0000

    me he cargado el npm

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require('morgan');
 const app = express();
 
-const moviesRouter  = require('./src/api/movies');
+const moviesRouter  = require('./api/movies');
 
 app.use(express.json());
 app.use('/movies', moviesRouter);
